148de162dbc40ff5d88ac21a85c4555c414137f7
Raju Mandavalli <raju.mandavalli@gmail.com>
2026-08-04 04:37:54 -0700

    Fix firebase config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,71 @@
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  updateProfile,
+  setPersistence,
+  browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+  addDoc,
+  updateDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFSnAwXw6uJB1QZjqT1IHlYBwsS9CsvLw",
+  authDomain: "mvr-properties-64922.firebaseapp.com",
+  projectId: "mvr-properties-64922",
+  storageBucket: "mvr-properties-64922.firebasestorage.app",
+  messagingSenderId: "1091310315390",
+  appId: "1:1091310315390:web:d7f79a87c0b1024059f52f"
+};
+
+// Initialize Firebase
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// Local persistence
+setPersistence(auth, browserLocalPersistence).catch((e) => {
+  console.warn("Could not set persistence:", e.message || e);
+});
+
+// Export functions
+export {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  updateProfile,
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+  addDoc,
+  updateDoc,
+  serverTimestamp
+};
