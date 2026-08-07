9fdfb6f95543ffb0b8eaf83ef184f1016c392370
Raju Mandavalli <raju.mandavalli@gmail.com>
2026-08-07 04:01:44 -0700

    Update SELLBY chat and details structure

--- js/home.js
+++ js/home.js
@@ -1,1 +1,29 @@
+import { auth, signOut } from "./firebase-config.js";
 
+const menuBtn = document.querySelector(".menu-btn");
+const sideMenu = document.getElementById("sideMenu");
+const logoutBtn = document.getElementById("logoutBtn");
+
+if (menuBtn && sideMenu) {
+
+    menuBtn.addEventListener("click", () => {
+
+        sideMenu.classList.toggle("show");
+
+    });
+
+}
+
+if (logoutBtn) {
+
+    logoutBtn.addEventListener("click", async (e) => {
+
+        e.preventDefault();
+
+        await signOut(auth);
+
+        window.location.href = "login.html";
+
+    });
+
+}
